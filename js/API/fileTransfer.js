f8a26deeeba0dd7c5c734b84595d0bf96e2d062f
erilke <erilkemx@gmail.com>
2015-10-31 10:13:24 -0600

    version2

--- js/API/fileTransfer.js
+++ js/API/fileTransfer.js
@@ -1,46 +1,22 @@
-var ft = {
-
+var fileTransfer = {
 	win: function (r) {
-		if(r.response){
-			//EXITOSO
-
-		navigator.notification.alert('Registrado correctamente',
-			function(){
-				navigator.vibrate(2000);
-				navigator.notification.beep(1);
-				window.localStorage.setItem('uuid',10);
-				window.location.href ="home";
-			},
-			'Bienvenido',
-			'Registrado'
-		);
-
-
+		if(r.response == '1'){
+			navigator.notification.alert("Los datos se han enviado satisfactoriamente", null, "Registro Correcto", "Aceptar");
 		}else{
 			alert("Error");
 		}
+			
 	},
-
 	fail: function (error) {
-		alert("Error "+error.code);
+		alert("An error has occurred: Code = " + error.code);
 	},
-
-	start: function(path){
-
+	sendPhoto: function(path){
 		var options = new FileUploadOptions();
 		options.fileKey = "foto";
-		options.fileName = "Edgar";
-		options.mimeType = "image/jpg";
+		options.fileName = "Carlos";
+		options.mimeType = "image/jpeg";
 
 		var ft = new FileTransfer();
-		ft.upload(path, encodeURI("http://carlos.igitsoft.com/apps/test.php"), ft.win, ft.fail, options);
-
+		ft.upload(path, "http://carlos.igitsoft.com/apps/test.php", fileTransfer.win, fileTransfer.fail, options);
 	}
 };
-
-
-
-
-
-
-
--- js/actions.js
+++ js/actions.js
@@ -1,84 +1,29 @@
 var fn = {
-    init:function(){
-       // alert();
-       // var x = false;
-        if (!fn.estarRegistrado())
-            window.location.href = '#registro';
-        $('#regSend').click(fn.getReg);
-        $('#tomarFoto').click(mc.start());
-
-    },
-
-    deviceready: function(){
-        document.addEventListener("deviceready", fn.init, false);
-    }
-
-    estarRegistrado: function(){
-  //      window.localStorage.getItem('uuid') !=undefined){
-  //          return true;
-  //      }
-        return false;
-    }
-
-    
-    getReg: function(){
-       // var nom=document.getElementById('regNom').value;
-
-        var nom = $('#regNom').val();
-        var tel = $('#regTel').val();
-        var mail = $('#regMail').val();
-        var foto = $('#fotoTomada').attr('rel');
-
-        if (nom!= '' && tel!= '' && mail!= '' && foto!= undefined && foto != ''){
-        //if (nom!= '' && tel!= '' && mail!= ''){
-
-            //alert(nom + '-' + tel + '-'+ mail);
-            fn.enviarRegistro(nom,mail,tel,foto);
-            //fn.enviarRegistro(nom,mail,tel);
-
-        }
-
-        else {
-            //alert("Todos los campos son requeridos");
-            navigator.notification.alert('Todos los campos son requeridos!');
-        }
-            
-
-
-
-    },
-
-    enviarRegistro: function(nombre,mail,telefono,foto){
-
-            $.ajax({
-                      method: "POST",
-                      url: "http://carlos.igitsoft.com/apps/test.php",
-                      data: { 
-                        nom: nombre,
-                        mail: mail,
-                        tel: telefono
-                    }
-            }).done(function(msg){
-                if(msg == 1 ){
-                    // Enviar foto
-                    ft.start(foto);
-                 }else{
-                    alert("Datos incorrectos");
-                }
-
-            });
-
-    }
-    
-    
+	init: function(){
+		if(!fn.islogged())
+			window.location.href = "#reg";
+		
+		//Funcionalidades de Registro
+		$('#regSend').click(fn.getRegister);
+		$('#takePhoto').click(mediaCapture.takePhoto);
+	},
+	ready:function(){
+		document.addEventListener("deviceready", fn.init, false);
+	},
+	islogged: function(){
+		return false;
+	},
+	getRegister: function(){
+		var nom = $('#regName').val();
+		var tel = $('#regTel').val();
+		var mail = $('#regMail').val();
+		var foto = $('#regPhoto').attr('rel');
+		if(nom != '' && tel != '' && mail != '' && foto != undefined && foto != ''){
+			server.regSend(nom, tel, mail, foto);
+		}else{
+			navigator.notification.alert('Todos los campos son requeridos', null, "Error de Registro", "Aceptar");
+		}
+	}
 };
-/*window.addEventListener("load",fn.init,false);*/
-/*jQuery(dcoument).ready(fn.init);*/
-//$(dcoument).ready(fn.init);
 
-
-//comentar línea de abajo cuando la app esté lista para compilar
-//$(fn.init);
-
-//descomentar línea de abajo cuando la app esté lista para compilar
-$(fn.deviceready);
+$(fn.ready);
