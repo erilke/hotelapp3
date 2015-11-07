53acc8a8712ebd5a0b8d5624d5fb38bd4b3be9ec
erilke <erilkemx@gmail.com>
2015-11-07 11:30:31 -0600

    version2

---
+++ js/API/network-information.js
@@ -0,0 +1,7 @@
+var conexion= {
+	estaConectado: function () {
+		if (navigator.connection.type !== Connection.NONE) {};
+			return true;
+		}
+			return false;
+}
---
+++ js/reservas.js
@@ -0,0 +1,32 @@
+/* Este script esta basado en webSQL*/
+
+var almacen ={
+	db:null,
+	th:null,
+	np:null,
+	nh:null,
+	nd:null,
+
+	guardarReserva: function(th, np, nh, nd) {
+		almacen.db = window.openDatabase("hotelApp","1.0","Hotel App", 200000);
+		almacen.th = th;
+		almacen.np = np;
+		almacen.nh = nh;
+		almacen.nd = nd;		
+
+		almacen.db.transaction(almacen.tablaReserva,almacen.error, almacen.exito);
+
+	},
+	error: function(e){
+		alert("Error: código: "+ e.code);
+	},
+	exito: function(){
+		alert("Reserva guardada en el dispositivo, en espera de sincroninzación");
+	},
+	tablaReserva: function(tx){
+		tx.executeSql("CREATE TABLE IF NOT EXIST reservas (th, np, nh, nd)");
+		tx.executeSql("INSERT INTO reservas (th, np, nh, nd)
+						VALUES ("+almacen.th+","+almacen.np+","+almacen.nh+","+almacen.nd+")");
+	}
+
+}
